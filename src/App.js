18f84c3d9cd82f5109a88b8f2de884902fe733a9
Shreya Singal <singalshreya97@gmail.com>
2024-06-15 12:00:17 +0530

    error fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { Editor, EditorState, RichUtils,Modifier,convertToRaw,convertFromRaw,DefaultDraftBlockRenderMap  } from 'draft-js';
 import Immutable from 'immutable';
